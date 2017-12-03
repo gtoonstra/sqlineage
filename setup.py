ee069d3f67456a44935463c6e5e07ea43a23dbd4
gtoonstra <gtoonstra@gmail.com>
2017-12-03 13:03:50 +0100

    Enriched setup before publishing

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 from distutils.core import setup, Extension
 
 sqlineage = Extension('sqlineage',
-                    define_macros = [('MAJOR_VERSION', '0'),
+                      define_macros=[('MAJOR_VERSION', '0'),
                                      ('MINOR_VERSION', '1')],
-                    include_dirs = ['/usr/local/include'],
-                    # libraries = ['tcl83'],
-                    library_dirs = ['/usr/local/lib'],
-                    # sources = ['src/sqlineage.c'])
-                    sources = ['src/sqlscanner.c', 'src/state_machine.c'])
+                      include_dirs=['/usr/local/include'],
+                      library_dirs=['/usr/local/lib'],
+                      sources=['src/sqlscanner.c', 'src/state_machine.c'])
 
-setup (name = 'sqlineage',
-       version = '0.1.1',
-       description = 'A parser for SQL',
-       author = 'G. Toonstra',
-       author_email = 'gtoonstra@gmail.com',
-       url = 'https://github.com/gtoonstra/sqlineage',
-       long_description = '''
+setup(name='sqlineage',
+      version='0.1.0',
+      description='A scanner for SQL',
+      author='G. Toonstra',
+      author_email='gtoonstra@gmail.com',
+      url='https://github.com/gtoonstra/sqlineage',
+      license='Apache2',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: ',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
+      ],
+      packages=['sqlineage'],
+      long_description='''
 This package parses SQL files and allows you to build a hierarchy
 of select and insert statements that lists the identifiers, so you
 can build a model that reflects data lineage in the statements and
 how data flows.
 ''',
-       ext_modules = [sqlineage])
+      ext_modules=[sqlineage])
